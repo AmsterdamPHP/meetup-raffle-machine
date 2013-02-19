4107d35d28efcb2069f2417a4609c7b9de897fb6
Frank van den Brink <frank@frankvandenbrink.nl>
2013-02-19 14:09:12 +0100

    Simplified raffle animations

--- web/js/raffler.js
+++ web/js/raffler.js
@@ -3,11 +3,26 @@
  */
 var Raffler = {
     /**
+     * The delay between highlighting each checkin.
+     */
+    highlightDelay: 0,
+
+    /**
+     * The current hightlight cycle.
+     */
+    currentCycle: 0,
+
+    /**
+     * Whether we are cycling.
+     */
+    cycling: false,
+
+    /**
      * Initialize
      */
     init: function() {
         $(document).on('keydown', function(e) {
-            if (e.keyCode == 32) {
+            if (e.keyCode == 32 && !Raffler.cycling) {
                 Raffler.raffle();
             }
         });
@@ -17,17 +32,52 @@ var Raffler = {
      * Raffle.
      */
     raffle: function () {
-        window.setInterval(Raffler.highlightRandomCheckin, 250);
+        // Reset state
+        Raffler.currentCycle = 0;
+        Raffler.highlightDelay = 0;
+
+        // Start cycling
+        Raffler.cycling = true;
+        Raffler.highlightRandomCheckin();
     },
 
     /**
-     * Get a random checkin.
+     * Highlight random checkin.
      */
     highlightRandomCheckin: function() {
-        var min = 0;
-        var max = $('.checkin').size() - 1;
-        var random = Math.floor(Math.random() * (max - min + 1)) + min;
-        $('.checkin').removeClass('selected');
-        return $('.checkin').eq(random).addClass('selected');
+        // Abort if we have reached 50 cycles
+        if (50 <= Raffler.currentCycle) {
+            Raffler.cycling = false;
+            return;
+        }
+
+        // Increase the current highlight cycle
+        Raffler.currentCycle++;
+
+        // Adjust the highlight delay
+        Raffler.highlightDelay = Math.pow(1.14, Raffler.currentCycle);
+
+        // Get random person to highlight
+        checkin = Raffler.getRandomCheckin();
+
+        // Highlight, delay, unhighlight
+        checkin.addClass('selected', Raffler.highlightDelay, Raffler.unhighlightCurrentCheckin);
+    },
+
+    /**
+     * Unhighlight current checkin
+     */
+    unhighlightCurrentCheckin: function() {
+        // Unhighlight this checkin and recurse back to highlighting another
+        // random checkin
+        $(this).removeClass('selected', Raffler.highlightDelay, Raffler.highlightRandomCheckin);
+    },
+
+    /**
+     * Get random checkin.
+     */
+    getRandomCheckin: function() {
+        var random = Math.floor(Math.random() * $('.checkin').size());
+        return($('.checkin').eq(random));
     }
 };
