130bcc4d29dc2ba0be176880f62d5598df057c8d
Dennis de Greef <dennis@transip.nl>
2015-01-16 11:06:32 +0100

    Make curl follow redirects to prevent relocation in the future

--- src/Raffle/RandomService.php
+++ src/Raffle/RandomService.php
@@ -25,6 +25,7 @@ class RandomService
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $data = curl_exec($ch);
 
         // Decode data and return
