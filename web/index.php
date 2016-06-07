865d63d004adcf2d61a84be6d73e21ca52cec09d
Rafael Dohms <rdohms@gmail.com>
2016-06-07 13:45:31 +0200

    Overhaul for builds
    - added gulp and node-sass to remove ruby deps
    - moved in compass replacements
    - added proper asset building with gulp
    - moved source stuff to source
    - added circle and deploy scripts, untested yet.

--- web/index.php
+++ web/index.php
@@ -4,6 +4,12 @@ ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+// For interal PHP server
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/dev.php';
 require __DIR__.'/../src/controllers.php';
