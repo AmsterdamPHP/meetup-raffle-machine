18794ad4be89ef86c5341cda662e66414bcff770
Dennis de Greef <github@link0.net>
2016-10-06 00:00:35 +0200

    Maybe the two rounds take longer than 30 seconds

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var Nightmare = require('nightmare');
 var expect = require('chai').expect;
 
 describe('AmsterdamPHP Raffler', function () {
-    this.timeout(30000); // Set timeout to 15 seconds, instead of the original 2 seconds
+    this.timeout(60000); // Set timeout to 15 seconds, instead of the original 2 seconds
     var url = 'http://localhost:8080',
         nightmare;
 
